68355c0f0b050c377435887d552f3649265f9ca6
Daiki Yamamoto <0ey329231k1095v@gmail.com>
2023-10-29 23:23:20 -0700

    modified api url

--- api/index.ts
+++ api/index.ts
@@ -1,65 +1,78 @@
-import express from 'express';
-const app = express();
+import express from 'express'
+const app = express()
 require('dotenv').config()
 const cors = require('cors')
 const mongoose = require('mongoose')
 const MONGODB_URL = process.env.MONGODB_URL
 
+const {
+  registerUser,
+  loginUser,
+  getUser,
+} = require('./controllers/User.controller')
+const {
+  createPlan,
+  getPlans,
+  getPlanById,
+  deletePlan,
+  updatePlan,
+} = require('./controllers/TourPlan.controller')
 
-const {registerUser, loginUser, getUser} = require('./controllers/User.controller')
-const {createPlan, getPlans,getPlanById , deletePlan, updatePlan} = require('./controllers/TourPlan.controller')
-
-app.use(cors({credentials: true, origin: 'http://localhost:5173'}));
+// app.use(cors({credentials: true, origin: 'http://localhost:5173'}));
+app.use(
+  cors({ credentials: true, origin: 'https://trip-planner-xi.vercel.app' })
+)
 mongoose.connect(MONGODB_URL).then(() => console.log('Connected to DB'))
 app.use(express.json())
 
-app.post("/register", registerUser)
-app.post("/login", loginUser)
-app.post("/user", getUser)
+app.post('/register', registerUser)
+app.post('/login', loginUser)
+app.post('/user', getUser)
 
-app.post("/plan", getPlans)
+app.post('/plan', getPlans)
 // app.get("/plan", getPlans)
-app.get("/plan/:id", getPlanById)
-app.post("/plan/create", createPlan)
-app.put("/plan/update/:id",updatePlan)
-app.delete("/plan/delete/:id", deletePlan)
+app.get('/plan/:id', getPlanById)
+app.post('/plan/create', createPlan)
+app.put('/plan/update/:id', updatePlan)
+app.delete('/plan/delete/:id', deletePlan)
 
-app.post("/getDirection", async(req, res) => {
+app.post('/getDirection', async (req, res) => {
+  const { origin, destination } = req.body
+  console.log(req.body)
 
-    const {origin, destination} = req.body
-    console.log(req.body);
-
-    const requestData = {
-        origin: {
-          location: {
-            latLng: {
-              latitude: origin.lat,
-              longitude: origin.lng
-            }
-          }
+  const requestData = {
+    origin: {
+      location: {
+        latLng: {
+          latitude: origin.lat,
+          longitude: origin.lng,
         },
-        destination: {
-          location: {
-            latLng: {
-              latitude: destination.lat,
-              longitude: destination.lng
-            }
-          }
-        }
-      };
+      },
+    },
+    destination: {
+      location: {
+        latLng: {
+          latitude: destination.lat,
+          longitude: destination.lng,
+        },
+      },
+    },
+  }
 
-    const data = await fetch(`https://routes.googleapis.com/directions/v2:computeRoutes?key=${process.env.GOOGLE_API_KEY}&$fields=*`, {
-        method: 'POST',
-        headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify(requestData)
-    })
-    const response = await data.json()
-    console.log(response);
-    res.json(response)
-  })
-  
-  
-  const PORT = process.env.PORT
-  app.listen(PORT, () => {
-    console.log(PORT,'start server');
+  const data = await fetch(
+    `https://routes.googleapis.com/directions/v2:computeRoutes?key=${process.env.GOOGLE_API_KEY}&$fields=*`,
+    {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(requestData),
+    }
+  )
+  const response = await data.json()
+  console.log(response)
+  res.json(response)
+})
+
+const PORT = process.env.PORT
+app.listen(PORT, () => {
+  console.log(PORT, 'start server')
 })
