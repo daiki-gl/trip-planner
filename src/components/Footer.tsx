7f03dfb545a568682c827a845955c51afc16417f
Daiki Yamamoto <0ey329231k1095v@gmail.com>
2023-10-31 23:56:45 -0700

    modified footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,7 @@
-import React from 'react'
-
 const Footer = () => {
   return (
     <footer className='bg-blue-900 text-center py-3 text-xs text-slate-100'>
-        <span>Copyright &copy; 2023 Yamamoto.</span>
+        <span>Copyright &copy; 2023 Trip Planner.</span>
     </footer>
   )
 }
